2945eb34f8c3bf43d0507d55ead8b84ac5d27381
rajen-thapa <63527798+rajen-thapa@users.noreply.github.com>
2021-08-16 10:20:01 +0545

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def elapsed():
 
 @app.route('/')
 def root():
-    return "Hello World (Python)! (up %s)\n" % elapsed()
+    return "Hello World (Python).I AM RAJENDRA! (up %s)\n" % elapsed()
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=4030)
